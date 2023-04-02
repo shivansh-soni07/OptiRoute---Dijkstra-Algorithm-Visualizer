fab50caad92fa3e2522c276826947940f0107395
Shivansh Soni <72219885+shivansh-soni07@users.noreply.github.com>
2023-04-03 02:19:23 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import PathfindingVisualizer from "./PathfindingVisualizer/PathfindingVisualizer.jsx";
+ 
+
+
+function App() {
+  return (
+    <div className="App">
+      <PathfindingVisualizer></PathfindingVisualizer>
+       
+    </div>
+    
+  );
+}
+
+export default App;
